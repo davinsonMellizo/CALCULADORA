1c80acb3a1d45246bd51de9f16a9fffb3f258831
JHONATAN <JHONATAN@10.130.5.149>
2018-08-27 10:53:12 -0500

    Pruebas

--- test/Pruebas/Prueba.java
+++ test/Pruebas/Prueba.java
@@ -5,6 +5,7 @@
  */
 package Pruebas;
 
+import calculator.CalculatorBean;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -36,10 +37,40 @@ public class Prueba {
     @After
     public void tearDown() {
     }
+    
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
+    @Test
+    public void testSuma()
+    {
+        System.out.println("suma");
+        int a = 4;
+        int b = 3;
+        CalculatorBean instance = new CalculatorBean();
+        int expResult = Integer.parseInt("7");
+        instance.setParam1(a);
+        instance.setParam2(b);
+        String result = instance.add();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+       @Test
+    public void testResta()
+    {
+        System.out.println("Resta");
+        int a = 4;
+        int b = 3;
+        CalculatorBean instance = new CalculatorBean();
+        int expResult = Integer.parseInt("1");
+        instance.setParam1(a);
+        instance.setParam2(b);
+        String result = instance.substract();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
     // public void hello() {}
 }
